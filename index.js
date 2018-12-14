bc8ed945e3feae043be727854665d2762a68b972
Robert Brown <brownrc@live.com>
2018-12-14 04:08:42 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3006;
+var ping = require('ping');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  console.log('Client connected from ' + socket.handshake.address);
+
+  socket.on('request-ping', function(params) {
+    setTimeout(() => {
+      ping.promise.probe(params.host, {
+        timeout: 10,
+        extra: ['-i 2']
+      }).then(function (res) {
+        if (res.alive) {
+          socket.emit('receive-ping', params.host + ' > Response made - ' + res.time + 'ms');
+        }
+        else {
+          socket.emit('receive-ping', 'Host unreachable.');
+        }
+      });
+    }, 750);
   });
 });
 
