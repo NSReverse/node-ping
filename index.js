5be9055ab6a9b02b8bf0fea319027ee6632fffbc
Robert Brown <brownrc@live.com>
2018-12-29 02:14:18 +0000

    Added logging socket interfaces

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 80;
-var ping = require('net-ping-hr');
+var fs =      require('fs');
+var path =    require('path');
+var express = require('express');
+var app =     express();
+var http =    require('http').Server(app);
+var io =      require('socket.io')(http);
+var port =    process.env.PORT || 80;
+var ping =    require('net-ping-hr');
 var session = ping.createSession();
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/logs', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/../APPLICATION_DATA/logs/index.html'));
+});
+
+app.use('/iconic', express.static(path.resolve(__dirname + '/../APPLICATION_DATA/logs/iconic/svg')));
 
 io.on('connection', function(socket){
   console.log('Client connected from ' + socket.handshake.address);
@@ -40,6 +49,34 @@ io.on('connection', function(socket){
           }
       });
     }, 750);
+  });
+
+  socket.on('ping-complete', function(logs) {
+    fs.writeFile('../APPLICATION_DATA/logs/log_' + new Date().getTime() + '.txt', logs, function(err, data) {
+      if (err) console.log(err);
+      io.emit('update-broadcast');
+    });
+  });
+
+  socket.on('request-listings', function() {
+    fs.readdir('../APPLICATION_DATA/logs', function(err, items) {
+      if (err) socket.emit('listings-error', err);
+      socket.emit('listings-received', items);
+    });
+  });
+
+  socket.on('request-read-log', function(filename) {
+    fs.readFile('../APPLICATION_DATA/logs/' + filename, 'utf-8', function(err, contents) {
+      if (err) socket.emit('listings-error', err);
+      socket.emit('request-read-log-complete', contents);
+    });
+  });
+
+  socket.on('request-delete-log', function(filename) {
+    fs.unlink('../APPLICATION_DATA/logs/' + filename, function(err) {
+      if (err) socket.emit('listings-error', err);
+      io.emit('update-broadcast');
+    });
   });
 });
 
